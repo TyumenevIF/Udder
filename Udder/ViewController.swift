37d46172a4753983fa4f278af18ba8f83a77cd98
Ilyas Tyumenev <tumenoff@mail.ru>
2023-04-09 12:19:32 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ilyas Tyumenev on 09.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
